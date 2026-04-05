fb949ac9e8528211756606f9b5f154f541b858d1
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2026-04-05 20:19:39 +0000

    Refactor repo and make it suitable for hyperion

---
+++ scripts/hyperion.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# update system & install core packages
+pacman -Syyu --no-confirm
+# Switch to pacman.conf from files 
+cp -r /tmp/files/pacman.conf /etc/pacman.conf
+pacman -Syyu --no-confirm
+# Install core packages 
+pacman -Sy --no-confirm jupiter-main/gamescope steam xdg-desktop-portal xdg-desktop-portal-kde xdg-desktop-portal-gamescope base-devel systemd
+# NOTE - SystemD is needed for ds-inhibit 
+# Install drivers - NVIDIA support should be baked in thorough distrobox
+pacman -Sy --no-confirm vulkan-intel vulkan-radeon vulkan-headers lib32-vulkan-radeon lib32-vulkan-intel
+# Install Steam and other gaming utilities through AUR and Chaotic AUR 
+pacman -Sy --no-confirm steam mangohud scx-scheds scx-tools
+# Install System-Wide fonts & Codecs
+pacman -Sy --no-confirm noto-fonts ttf-dejavu ttf-liberation noto-fonts-emohi noto-fonts-cjk
+# Codecs
+pacman -Sy --no-confirm flac wavpack lame libmad faac libwebp libavif libheif libjxl aom libde265 x264
+
+# Setup Chaotic AUR
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB 
+pacman -U --no-confirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U --no-confirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+cp -r /tmp/files/pacman.conf.chaotic /etc/pacman.conf
+pacman -Syyu
+
+# Install AUR and gaming packages
+sudo pacman -S paru heroic-games-launcher-bin faugus-launcher proton-cachyos proton-cachyos-slr protonplus bottles gamescope-git mesa-tkg-git paru
+
+
+# Switch & Create build user - Similar to Docker 
+sudo useradd -m -s -G /bin/bash builduser wheel
+su builduser
+# As builduser
+# make temp build dir 
+cd ~ && mkdir -p ~/tmp/build-aur 
+cd ~/tmp/build-aur
+paru -S --no-confirm scopebuddy scopebuddy-gui-git ds-inhibit goverlay-git
+# Cleanup 
+paru -Rns --no-confirm $(paru -Qtdq)
+paru -Scc --no-confirm
+sudo su #Switch back to root
+pkill -u builduser
+userdel -r builduser
